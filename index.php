5498d290943960ee754f50bb68af854dc3659e77
Imsmthng <agustin.cesara13@gmail.com>
2021-11-06 10:48:49 +0800

    11/06/2021

--- index.php
+++ index.php
@@ -3,7 +3,19 @@ include('includes/header.php');
 include('includes/navigation.php');
 ?>
 
-
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow">
+                    <div class="card-header text-center">
+                        Add New User
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php 
